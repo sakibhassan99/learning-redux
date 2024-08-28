06500b7f6cf0c8e30dc69fa4050eb6e779f74902
Sakib Hassan <82075395+sakibhassan99@users.noreply.github.com>
2024-08-29 00:08:05 +0600

    Making Shopping Cart

---
+++ Making Shopping Cart/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import CartIcon from "../assets/cart-icon.svg";
+import { useSelector } from "react-redux";
+
+export default function Header() {
+  const cartItems = useSelector((state) => state.cartItems);
+  const cartCount = cartItems.reduce((acc, curr) => acc + curr.quantity, 0);
+  return (
+    <header>
+      <div className="header-contents">
+        <h1>
+          <Link to="/">Shopee</Link>
+        </h1>
+        <div>
+          <Link className="cart-icon" to="/cart">
+            <img src={CartIcon} alt="cart-icon" />
+            <div className="cart-items-count">{cartCount}</div>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ Making Shopping Cart/readme.md
@@ -0,0 +1,4 @@
+# Making a simple ecommerce website
+
+making a simple e-commerce website it is very simple im just building it
+to understand redux more good and know how to works with complex state management
