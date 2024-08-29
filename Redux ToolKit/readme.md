3296d9a9156385b0cdc0779ded47bd102b27dd5e
Sakib Hassan <82075395+sakibhassan99@users.noreply.github.com>
2024-08-30 00:41:45 +0600

    Redux ToolKit

---
+++ Redux ToolKit/readme.md
@@ -0,0 +1,6 @@
+# Redux ToolKit
+
+so i just learned about redux toolkit how its works how configure store works
+how can i give multiple reducers in configure store. also learned about
+how create slice works how it creates action types, action creators, reducer
+all together and how to create slices as well
---
+++ Redux ToolKit/store/slices/cartSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const findItemIndex = (state, action) =>
+  state.findIndex((item) => item.productId === action.payload.productId);
+
+const slice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addCartItem(state, action) {
+      const existingItemIndex = findItemIndex(state, action);
+      if (existingItemIndex !== -1) {
+        state[existingItemIndex].quantity += 1;
+      } else {
+        state.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeCartItem(state, action) {
+      const existingItemIndex = findItemIndex(state, action);
+      state.splice(existingItemIndex, 1);
+    },
+    increaseCartItemQuantity(state, action) {
+      const existingItemIndex = findItemIndex(state, action);
+      if (existingItemIndex !== -1) {
+        state[existingItemIndex].quantity += 1;
+      }
+    },
+    decreaseCartItemQuantity(state, action) {
+      const existingItemIndex = findItemIndex(state, action);
+      if (existingItemIndex !== -1) {
+        state[existingItemIndex].quantity -= 1;
+        if (state[existingItemIndex].quantity === 0) {
+          state.splice(existingItemIndex, 1);
+        }
+      }
+    },
+  },
+});
+
+export default slice.reducer;
+export const {
+  addCartItem,
+  removeCartItem,
+  increaseCartItemQuantity,
+  decreaseCartItemQuantity,
+} = slice.actions;
