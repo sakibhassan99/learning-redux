f9546a169a2df62036e6ac06882eb18b74e91fd1
Sakib Hassan <82075395+sakibhassan99@users.noreply.github.com>
2024-08-29 15:42:26 +0600

    Immer Js With Redux

---
+++ Immer Js With Redux/components/CartItem.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useDispatch } from "../react-redux.js";
+import {
+  decreaseCartItemQuantity,
+  increaseCartItemQuantity,
+  removeCartItem,
+} from "../store/slices/cartSlice.js";
+
+export default function CartItem({
+  productId,
+  title,
+  rating,
+  price,
+  imageUrl,
+  quantity,
+}) {
+  const dispatch = useDispatch();
+  return (
+    <div className="cart-item-container">
+      <div className="cart-item">
+        <img src={imageUrl} alt={title} />
+        <div>
+          <h3>{title}</h3>
+          <p>{rating} ★ ★ ★ ★</p>
+        </div>
+      </div>
+      <div className="item-price">${price}</div>
+      <div className="item-quantity">
+        <button
+          onClick={() => {
+            dispatch(decreaseCartItemQuantity(productId));
+          }}
+        >
+          -
+        </button>
+        <span>{quantity}</span>
+        <button
+          onClick={() => {
+            dispatch(increaseCartItemQuantity(productId));
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            dispatch(removeCartItem(productId));
+          }}
+        >
+          Remove Item
+        </button>
+      </div>
+      <div className="item-total">${(quantity * price).toFixed(2)}</div>
+    </div>
+  );
+}
---
+++ Immer Js With Redux/readme.md
@@ -0,0 +1,7 @@
+# Immer js with Redux
+
+so i used immer js with redux to use mutable format so i don't have to
+use the immutable format to spread every single time and add new items
+by using immer i can use the mutable way and push new items directly to
+the array so what immer does it basically create a new object or array and
+return that so it works as immutable way
